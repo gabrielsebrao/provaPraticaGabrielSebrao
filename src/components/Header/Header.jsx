47ccc9410a6fee95eb34d9e2558600a7373421bd
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-04 23:29:58 -0300

    feat: add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <img src='./logo-senai.png'/>
+            <h1>Gabriel de Oliveira Sebrão - 3B</h1>
+        </header>
+    )
+}
