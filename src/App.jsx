7e6ba3ef9777e3c981b13f6b8aa7eaa79a0f0488
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-07-04 23:30:25 -0300

    feat: add clubs list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import './App.css'
+import Header from './components/Header/Header'
+import Clubs from './components/Clubs/Clubs'
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <Clubs />
     </>
   )
 }
---
+++ src/components/Clubs/Clubs.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+export default function Clubs() {
+    const [clubes, setClubes] = useState([]);
+    const [erro, setErro] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://api.cartola.globo.com/clubes`);
+            const data = await response.json();
+            const clubesList = Object.values(data);
+            setClubes(clubesList);
+        } catch (error) {
+            setErro(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {erro && <p>Ocorreu um erro: {erro.message}</p>}
+            <ul>
+            {clubes.slice(1).map((clube) => (
+                    <li key={clube.id}>
+                    <div>
+                        <img
+                            src={clube.escudos['60x60']}
+                            alt={`${clube.nome} logo`}
+
+                        />
+                        <div>
+                            <p>{clube.nome}</p>
+                            <p>{clube.apelido}</p>
+                        </div>
+                    </div>
+                </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
